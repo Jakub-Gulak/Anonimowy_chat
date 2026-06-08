ae37152efa7f67ea57e0bf76360d4dcd39c1bc6e
Jakub-Gulak <jakubgulak@icloud.com>
2026-06-08 17:39:53 +0200

    add hashing for admin

--- backend/init.sql
+++ backend/init.sql
@@ -12,3 +12,10 @@ CREATE TABLE admins (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+-- Konto administratora
+INSERT INTO admins (username, password)
+VALUES (
+    'admin',
+    '$2a$12$Xz6u9qwbidw6NbhLdzznb.d41z8D2F.rPZPgpTYfCVaxaXnhmDgNW'
+);
